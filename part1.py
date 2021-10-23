26232f561f4d7e0ebd7caa3c18a7727d63eddd4d
John Emmanuel Palijado <palihotdog@gmail.com>
2021-10-23 19:43:47 +0800

    First part successful

---
+++ part1.py
@@ -0,0 +1,5 @@
+name = input("Enter you name: ")
+age = input("Enter your age: ")
+address = input("Enter you address: ")
+
+print(f"Hi, my name is {name}. I am {age} years old and I live in {address}.")
